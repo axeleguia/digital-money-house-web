173bab0fa3c05868c20407debda7834cdd09f735
Axel Leguia <axeleguia.chero@outlook.com>
2024-07-15 19:49:47 -0500

    Revert img until width can be set dinamycally

--- src/app/(auth)/register/success/page.tsx
+++ src/app/(auth)/register/success/page.tsx
@@ -6,7 +6,7 @@ const SuccessPage = () => {
   return (
     <div className={styles.card}>
       <h2 className={styles.cardTitle}>Registro Exitoso</h2>
-      <Image src="/success.svg" alt="Success" className={styles.cardIcon} />
+      <img src="/success.svg" alt="Success" className={styles.cardIcon} />
       <span className={styles.cardMessage}>
         Hemos enviado un correo de confirmación para validar tu email, por favor
         revisalo para iniciar sesión
